e9c863588e5b905c1f17cadaf7cdb85671166e63
Austin <aharshbe@live.com>
2018-04-10 13:58:35 -0700

    adding number 9

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    if (number < 0):
+        number = (abs(number))
+        number %= 10
+        print("{}".format(number), end='')
+        return (number)
+    else:
+        number %= 10
+        print("{}".format(number), end='')
+        return (number)
