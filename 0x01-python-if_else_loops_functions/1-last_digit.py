dba99cfc5ef1f633dc7ddee7184520af60ec4b8e
Austin <aharshbe@live.com>
2018-04-10 13:18:34 -0700

    adding number 0 and 1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,15 +2,15 @@
 import random
 number = random.randint(-10000, 10000)
 if (number < 0):
-    x = abs(number)
+    x = number % -10
 else:
-    x = number
+    x = number % 10
 if ((x % 10) > 5):
     print("Last digit of {} is {} and is greater than 5"
-          .format(number, x % 10))
-elif ((x % 10) == 0):
+          .format(number, x))
+elif (not (x)):
     print("Last digit of {} is {} and is 0"
-          .format(number, x % 10))
-elif ((x % 10) < 6 and (not 0)):
+          .format(number, x))
+elif ((x) < 6 and (not 0)):
     print("Last digit of {} is {} and is less than 6 and not 0"
-          .format(number, x % 10))
+          .format(number, x))
