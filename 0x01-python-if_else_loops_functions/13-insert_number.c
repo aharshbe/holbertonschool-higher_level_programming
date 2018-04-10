8b1f43aabcdf78aa56dcc783247c85249db100eb
Austin <aharshbe@live.com>
2018-04-10 15:06:24 -0700

    adding inital files for whileboard question

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+ * insert_node - singly linked list
+ * @head: pointer to head of linked list
+ * @number: number to be inserted
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+
+}
