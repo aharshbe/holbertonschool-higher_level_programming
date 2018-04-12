9a041fa4b466a6f0a0d020a49f0c6c5d441d6ded
Austin Harshberger <aharshbe@live.com>
2018-04-12 13:16:40 -0700

    adding changes to logic and removing the exit() to pass checker

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
-if (__name__ == '__main__'):
+if __name__ == '__main__':
     from sys import argv
-    len_of_arr = len(argv)
-    len_of_arr -= 1
+    len_of_arr = len(argv) - 1
 
-    if (len_of_arr == 0):
+    if (not len_of_arr):
         print("{} arguments.".format(len_of_arr))
-        exit()
     elif (len_of_arr == 1):
         print("{} argument: ".format(len_of_arr))
     else:
         print("{} arguments: ".format(len_of_arr))
-    j = 1
+    j = 1 if len_of_arr else 0
     for i in argv[1:]:
-        print("{}: {}".format(j, i))
-        j += 1
+        if (j >= 1):
+            print("{}: {}".format(j, i))
+            j += 1
