96a0b7bd52749ec3381723537029e5c02f5cc6e4
Austin Harshberger <aharshbe@live.com>
2018-04-12 12:52:56 -0700

    changing some things around

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
 if (__name__ == '__main__'):
     from sys import argv
-    j = 1
+
     len_of_arr = len(argv)
+
     if (len_of_arr == 1):
         print("0 arguments.")
+    elif (len_of_arr == 2):
+        len_of_arr -= 1
+        print("{} argument: ".format(len_of_arr))
     else:
         len_of_arr -= 1
-        if (len_of_arr == 1):
-            print("{} argument:".format(len_of_arr))
-        else:
-            print("{} arguments:".format(len_of_arr))
-            for i in argv[1:]:
-                print("{}: {}".format(j, i))
-                j += 1
+        print("{} arguments: ".format(len_of_arr))
+    j = 1
+    for i in argv[1:]:
+        print("{}: {}".format(j, i))
+        j += 1
