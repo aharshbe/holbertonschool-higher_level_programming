d072b3f40e60d95aabda89aa88220d54086c8c09
Austin Harshberger <aharshbe@live.com>
2018-04-12 14:44:50 -0700

    adding 103

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def magic_calculations(a, b):
-    from magic_calculaion_102 import add, sub
+    from magic_calcultaion_102 import add, sub
     if (a < b):
         c = add(a, b)
         for i in range(4, 6):
