02aa3d38a376991dd2facd664362aa2806a63d2d
Austin Harshberger <aharshbe@live.com>
2018-04-12 17:17:46 -0700

    adding some fancyness to the caculator because why not

--- 0x02-python-import_modules/prior.py
+++ 0x02-python-import_modules/prior.py
@@ -2,16 +2,22 @@
 if __name__ == "__main__":
     from sys import argv
     from calculator_1 import add, sub, mul, div
-    operators, f = ['+', '-', '*', '/'], [add, sub, mul, div]
+    operators = ['+', '-', '*', '/']
+    f = [add, sub, mul, div]
     if (len(argv) != 4):
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     else:
-        n1, n2, op, j, r = int(argv[1]), int(argv[3]), argv[2], 0, 0
-        for j, i in enumerate(operators):
+        n1 = int(argv[1])
+        n2 = int(argv[3])
+        op = argv[2]
+        j = 0
+        r = 0
+        for i in operators:
             if (op == i):
                 print("{} {} {} = {}".format(n1, i, n2, f[j](n1, n2)))
                 r = 1
+            j += 1
         if (not r):
             print("Unknown operator. Available operators: +, -, * and /")
             exit(1)
