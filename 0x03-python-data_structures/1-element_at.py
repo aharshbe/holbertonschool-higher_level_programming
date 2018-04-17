510464d00f19279baf92a7391bb2bc8e9c20e201
Austin <aharshbe@live.com>
2018-04-17 13:12:46 -0700

    adding number 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     if (idx < 0 or idx > len(my_list)):
-        return (None)
+        return
     for j, i in enumerate(my_list):
         if (j == idx):
             break
