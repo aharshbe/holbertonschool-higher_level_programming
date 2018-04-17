36acf584e474e79e7071a9e6610f0d7c7fbf272a
Austin <aharshbe@live.com>
2018-04-17 15:11:27 -0700

    setting a to first elemet in array

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     if my_list:
-        a = 0
+        a = my_list[0]
         for j in my_list:
-            if (not a > j):
+            if (a > j):
+                continue
+            else:
                 a = j
         return a
     return
