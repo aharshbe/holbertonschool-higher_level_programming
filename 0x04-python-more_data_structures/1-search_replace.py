64c1f25ff54c84fe3eafb667ad7661c6b486a398
Austin <aharshbe@live.com>
2018-04-19 15:22:33 -0700

    adding 1 with a single line

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = my_list.copy()
-    new_list[new_list.index(search)] = replace
-    return new_list
+    return [replace if k == search else k for k in my_list]
