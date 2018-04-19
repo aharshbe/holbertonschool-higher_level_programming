d9ff0a0d8175908e547776595dfd850d3231eec8
Austin <aharshbe@live.com>
2018-04-19 16:30:10 -0700

    adding 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -5,7 +5,7 @@ def best_score(a_dictionary):
         for i in a_dictionary:
             if (x < a_dictionary[i]):
                 x = a_dictionary[i]
-                person  = i
+                person = i
             else:
                 pass
         return person
