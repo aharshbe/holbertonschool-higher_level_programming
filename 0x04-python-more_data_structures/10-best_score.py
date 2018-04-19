b8ac67bd1546103e3802202f3c82d762b1f5797d
Austin <aharshbe@live.com>
2018-04-19 16:29:33 -0700

    adding 10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    x = 0
+    if a_dictionary:
+        for i in a_dictionary:
+            if (x < a_dictionary[i]):
+                x = a_dictionary[i]
+                person  = i
+            else:
+                pass
+        return person
+    return None
