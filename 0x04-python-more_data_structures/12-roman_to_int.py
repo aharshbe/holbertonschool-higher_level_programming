4004e19e117721ed32dfdb847207dae61fcc7f14
Austin <aharshbe@live.com>
2018-04-19 17:50:31 -0700

    fixing whiespace issue

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     if (type(roman_string) is str):
-        r = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+        r = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
         b = x = 0
         for p in roman_string:
             if (b < r[p]):
