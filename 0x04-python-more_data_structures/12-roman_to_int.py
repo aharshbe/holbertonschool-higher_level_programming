64fa77d9d12603fb0809e78c71a98faff2f7a411
Austin <aharshbe@live.com>
2018-04-19 17:45:01 -0700

    adding 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-xef roman_to_int(roman_string):
+def roman_to_int(roman_string):
     r = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'x':500, 'M':1000}
     b = x = 0
     for p in roman_string:
