99e4da015a0c7c0425bacd1183e01fc106ca4c6e
Austin <aharshbe@live.com>
2018-05-01 17:32:32 -0700

    adding main with property attribute

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -2,31 +2,28 @@
 class Square:
     """This class defines a square"""
     def __init__(self, size=0):
-        try:
-            if (size < 0):
-                raise ValueError("size must be >= 0")
-            else:
-                self.size = size
-        except TypeError:
+        if (type(size) is not int):
             raise TypeError("size must be an integer")
+        elif (size < 0):
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
 
+    @property
     def size(self):
         """Returns size square"""
-        return (size)
+        return (self.__size)
 
+    @size.setter
     def size(self, value):
         """Sets value of square's size"""
-        try:
-            if (value < 0):
-                raise ValueError("size must be >= 0")
-            else:
-                self.size = value
-        except TypeError:
+        if (type(value) is not int):
             raise TypeError("size must be an integer")
+        elif (value < 0):
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
 
     def area(self):
         """Return area of square"""
-        try:
-            return (self.size ** 2)
-        except TypeError:
-            raise TypeError("size must be an integer")
+        return (self.__size ** 2)
