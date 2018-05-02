cdc971f5bc7805f92ba3129cd6047bb8ac248007
Austin <aharshbe@live.com>
2018-05-01 17:14:00 -0700

    fixing commends

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -11,12 +11,12 @@ class Square:
             raise TypeError("size must be an integer")
         try:
             if (position[0] < 0 or position[1] < 0):
-                raise TypeError("position must be a tuple of"
+                raise TypeError("position must be a tuple of "
                                 "2 positive integers")
             else:
                 self.position = position
         except TypeError:
-            raise TypeError("position must be a tuple of"
+            raise TypeError("position must be a tuple of "
                             "2 positive integers")
 
     def size(self):
@@ -68,15 +68,15 @@ class Square:
     def position(self, value):
         """Offset position of square"""
         if (type(value) is not tuple):
-            raise TypeError("position must be a tuple of"
+            raise TypeError("position must be a tuple of "
                             "2 positive integers")
         elif (len(value) != 2):
-            raise TypeError("position must be a tuple of"
+            raise TypeError("position must be a tuple of "
                             "2 positive integers")
         elif (not(type(value[0]) is int and type(value[1])) is int):
-            raise TypeError("position must be a tuple of"
+            raise TypeError("position must be a tuple of "
                             "2 positive integers")
         elif value[0] < 0 or value[1] < 0:
-            raise TypeError("position must be a tuple of"
+            raise TypeError("position must be a tuple of "
                             "2 positive integers")
         self.position = value
