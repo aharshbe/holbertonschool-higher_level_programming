bed162dbe76a91f2c076d3322143ee260b2f3486
Austin <aharshbe@live.com>
2018-05-02 14:41:41 -0700

    adding number 2

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+    Print a name to stdout
+
+
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+        print a name
+    """
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+    if (not last_name):
+        print("My name is {}".format(first_name))
+    else:
+        print("My name is {} {}".format(first_name, last_name))
