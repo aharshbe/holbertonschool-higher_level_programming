94ef148e0a56bf19dbea3390ea30c21aaafdc8a3
Austin <aharshbe@live.com>
2018-05-02 12:53:49 -0700

    fixing pep errors

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,21 +1,23 @@
 #!/usr/bin/python3
-""" 
+"""
     My calculation module
 
 
 """
+
+
 def add_integer(a, b=98):
-	""" 
-    add_integer: add two ints
-	"""
-	if type(a) is float:
-		a = int(a)
-	if type(b) is float:
-		b = int(b)
+    """
+        add_integer: add two ints
+    """
+    if type(a) is float:
+        a = int(a)
+    if type(b) is float:
+        b = int(b)
 
-	if type(a) is not int:
-		raise TypeError("a must be an integer")
-	if type(b) is not int:
-		raise TypeError("b must be an integer")
+    if type(a) is not int:
+        raise TypeError("a must be an integer")
+    if type(b) is not int:
+        raise TypeError("b must be an integer")
 
-	return a + b
+    return a + b
