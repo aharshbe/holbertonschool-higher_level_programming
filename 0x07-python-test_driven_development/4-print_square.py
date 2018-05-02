9232c428878c8b9bf90e490154eed685f65c7593
Austin <aharshbe@live.com>
2018-05-02 14:55:06 -0700

    adding number 3

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+    Print a square w/#'s to stdout
+
+
+"""
+
+
+def print_square(size):
+    """
+        print a square
+    """
+    if type(size) is float and size < 0:
+        raise TypeError("size must be an integer")
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for i in range(size):
+        for i in range(size):
+            print("#", end='')
+        print()
