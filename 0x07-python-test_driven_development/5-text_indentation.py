1d8da468e485128976cd44bcb09a3f12532c389e
Austin <aharshbe@live.com>
2018-05-04 12:16:18 -0700

    adding number 4

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-    Prints a text with 2 new lines after 
-	special character
+    Prints a text with 2 new lines after
+    special character
 
 """
 
@@ -11,10 +11,10 @@ def text_indentation(text):
         Replace special character with 2 new lines
     """
     if type(text) is not str:
-    	raise TypeError("text must be a string")
-    
+        raise TypeError("text must be a string")
+
     for ch in [':', '.', '?']:
-    	if ch in text:
-    		text = text.replace(ch, ch+'\n')
+        if ch in text:
+            text = text.replace(ch, ch+'\n')
     s = [s.strip() for s in text.split('\n')]
     print("\n\n".join(s), end='')
