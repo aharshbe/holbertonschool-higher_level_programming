f17a1d12509002ef79a480737f1d648de3ab8135
Austin <aharshbe@live.com>
2018-05-04 16:12:56 -0700

    fixing pep errors

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -4,13 +4,16 @@
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
+
 class TestMaxInteger(unittest.TestCase):
+    """
+    unit test for max int function
+    """
+    def test_empty(self):
+        self.assertEqual(max_integer([]), None)
 
-  def test_empty(self):
-    self.assertEqual(max_integer([]), None)
+    def test_regular(self):
+        self.assertEqual(max_integer([1, 2, 3]), 3)
 
-  def test_regular(self):
-    self.assertEqual(max_integer([1, 2, 3]), 3)
-
-  def test_negative(self):
-    self.assertEqual(max_integer([-1, -2, -3]), -1)
+    def test_negative(self):
+        self.assertEqual(max_integer([-1, -2, -3]), -1)
