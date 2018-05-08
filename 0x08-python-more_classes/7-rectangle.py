afd68ca06ec7289bcfb2c168f5a1c6dc36969338
Austin <aharshbe@live.com>
2018-05-07 19:59:21 -0700

    adding tests for 7

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -69,4 +69,4 @@ class Rectangle:
 
     def __del__(self):
         Rectangle.number_of_instances -= 1
-        print("Bye rectangle…")
+        print("Bye rectangle...")
--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -69,7 +69,7 @@ class Rectangle:
 
     def __del__(self):
         Rectangle.number_of_instances -= 1
-        print("Bye rectangle…")
+        print("Bye rectangle...")
 
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
