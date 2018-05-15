2e0728b21eca5997d4dc9d4dd5156d8b485eed42
Austin <aharshbe@live.com>
2018-05-15 14:12:18 -0700

    adding 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""geometry module"""
+
+
+class BaseGeometry:
+    """geometry class"""
+    def area(self):
+        """method that has no content
+        Raises:
+            Excpetion: that it isn't implemented
+        """
+        raise Exception("area() is not implemented")
