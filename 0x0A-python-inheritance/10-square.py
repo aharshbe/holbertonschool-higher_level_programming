48d33ddeaebd27fe2676d5974df67b81bcd2cd28
Austin <aharshbe@live.com>
2018-05-15 15:27:25 -0700

    adding 10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""a square class"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """creates a square
+    Args:
+        BaseGeometry: inherits from given class
+    """
+    def __init__(self, size):
+        """initializer method
+        Args:
+            size: size
+        Attributes:
+            __size: private size
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(self.__size, self.__size)
