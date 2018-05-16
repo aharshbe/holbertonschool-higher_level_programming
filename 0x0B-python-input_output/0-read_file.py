af3e4ab8a581518a16ca96938764cfc50ba1baba
Austin <aharshbe@live.com>
2018-05-16 13:30:19 -0700

    adding comment to 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def read_file(filename=""):
+    """read in a file
+    Args:
+        filename: the file's name
+    """
     with open(filename, 'r') as f:
         content = f.read()
     print(content)
