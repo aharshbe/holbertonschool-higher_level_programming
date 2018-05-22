173477a03378c6006bf53c0e71f3fce1de640a59
austin <aharshbe@live.com>
2018-05-22 13:41:40 -0700

    adding 11

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -29,3 +29,26 @@ class Square(Rectangle):
             raise TypeError("width must be an integer")
         else:
             self.__size = value
+
+    def update(self, *args, **kwargs):
+        ''' updates any arguments passed '''
+        if (args):
+            for i, j in enumerate(args):
+                if i == 0:
+                    self.id = j
+                if i == 1:
+                    self.__size = j
+                if i == 2:
+                    self.__x = j
+                if i == 3:
+                    self.__y = j
+        else:
+            for i in kwargs:
+                if i == "size":
+                    self.__size = kwargs[i]
+                if i == 'x':
+                    self.__x = kwargs[i]
+                if i == 'y':
+                    self.__y = kwargs[i]
+                if i == 'id':
+                    self.id = kwargs[i]
