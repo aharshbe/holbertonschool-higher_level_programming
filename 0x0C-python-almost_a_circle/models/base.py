f53a760b88b14e9b41ea12263aba38c12cc35be2
austin <aharshbe@live.com>
2018-05-22 15:21:20 -0700

    adding 15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+import json
 
 ''' This file creates a base class '''
 
@@ -14,3 +15,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        ''' Convert to JSON string '''
+        if list_dictionaries is None or []:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
